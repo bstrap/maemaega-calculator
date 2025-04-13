47f74b2a6ffb10e9212c887a2776cd0b2a1f69d1
bstrap <hojaecho@gmail.com>
2025-04-13 22:21:55 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'maemaega-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// 설치 단계
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 요청 가로채기
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
